1053912db0f2a52aa7c9bfe19751d69541d66ad7
p208p2002 <p208p2002@gmail.com>
2024-03-27 10:51:39 +0800

    feat: add new test case

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,7 @@ mod test_tokenizer {
 
 #[cfg(test)]
 mod test_parser {
-    use crate::parser::{Event as ParserEvent, WikitextTableParser};
+    use crate::parser::{Event, WikitextTableParser};
     use std::fs::File;
     use std::io::Read;
 
@@ -46,7 +46,7 @@ mod test_parser {
         let wikitext_table_parser = WikitextTableParser::new(&content);
         for event in wikitext_table_parser {
             match event {
-                ParserEvent::RowStyle(row_style) => {
+                Event::RowStyle(row_style) => {
                     if count_rows > 0 {
                         // do not work just after parse the first row, which is a table headr.
                         assert_eq!(expect_cols, count_cols);
@@ -55,18 +55,43 @@ mod test_parser {
                     count_cols = 0;
                     println!("----- {:?} -----", row_style);
                 }
-                ParserEvent::Col(text) => {
+                Event::Col(text) => {
                     count_cols += 1;
                     println!("col: {:?}#", text);
                 }
-                ParserEvent::TableStart => count_table_start += 1,
-                ParserEvent::TableEnd => count_table_end += 1,
+                Event::TableStart => count_table_start += 1,
+                Event::TableEnd => count_table_end += 1,
                 _ => {}
             }
         }
         assert_eq!(expect_rows, count_rows);
         assert_eq!(1, count_table_start);
         assert_eq!(1, count_table_end)
+    }
+
+    fn test_table_caption(path:String,expect_caption:String) {
+        // read table from text
+        let mut file = match File::open(&path) {
+            Ok(file) => file,
+            Err(_) => {
+                return;
+            }
+        };
+        let mut content: String = String::new();
+        if let Err(_) = file.read_to_string(&mut content) {
+            return;
+        }
+
+        let wikitext_table_parser = WikitextTableParser::new(&content);
+        for event in wikitext_table_parser {
+            match event {
+                Event::TableCaption(caption)=>{
+                    assert_eq!(caption,expect_caption);
+                }
+                _ => {}
+            }
+        }
+    
     }
 
     #[test]
@@ -92,5 +117,12 @@ mod test_parser {
     #[test]
     fn test_parse_struct_table_5() {
         test_parse_struct_table(String::from("wikitext_tables/5.txt"), 14, 7)
+    }
+
+    #[test]
+    fn test_table_caption_1() {
+        let path = String::from("wikitext_tables/1.txt");
+        let expect_caption = "Seawater elemental composition<br>(salinity = 3.5%) {{citation needed|date=June 2019}}";
+        test_table_caption(path, expect_caption.to_string())
     }
 }
